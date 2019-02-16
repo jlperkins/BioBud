914a61998147430aff405598ddde819bdbaa465b
XCodeClub <xcodeclub@xcodeclubs-mac-399.local>
2019-02-16 16:34:49 -0500

    updates and basic setup changes 00

---
+++ Bio Bud/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Bio Bud
+//
+//  Created by XCodeClub on 2019-02-16.
+//  Copyright © 2019 XCodeClub. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+}
